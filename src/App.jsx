fc3ed1b77a4cc9fd4f2e7db9ef6d0fdaf50d8a1c
paulclrt <p4ul.claret@gmail.com>
2026-05-01 15:42:45 +0200

    [WIP] Testing github workflows for CI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import FinanceLandingPage from "./FinanceLandingPage.jsx"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   return <FinanceLandingPage />
 }
 
